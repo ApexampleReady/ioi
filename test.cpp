18781e525c5f1050aab9fa1f018d4b98735046c1
Bokai427 <fish042700@gmail.com>
2023-09-14 21:26:20 +0800

    Initial Commit.

---
+++ test.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+  ll n;
+  ll k;
+  cin >> n >> k; 
+  int max = -INF 
+  int secmax = -INF
+
+  for(int i = 0; i<n;i++ ){
+    int x;
+    cin >> x;
+    if(x>max){
+      max = x;
+    }
+    else if(x>secmax){
+      secmax = x;
+    }
+  } 
+  cout << secmax <<"\n";
+
+
+}
