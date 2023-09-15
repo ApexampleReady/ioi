cea2230edc60fd0d499112f8d89186c85fb73fff
TingKai Chiu <60634933+Bokai427@users.noreply.github.com>
2023-09-15 15:16:09 +0000

    1294a + 71a on codeforce

---
+++ codeforce/71a.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    ll n;
+    cin >> n;
+    for(int i = 0; i<n;i++ ){
+        string input;
+        cin >> input;
+        if(input.length() <= 10) {
+            cout << input << "\n";
+        }
+        else{
+            cout << input[0] << input.size()-2 << input[input.length()-1] << "\n";
+        }
+    }
+
+    return 0;
+}
